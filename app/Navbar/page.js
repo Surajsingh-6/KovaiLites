0057f8d3b53e4c2b65897c99b86a4924b144ef51
Suraj singh <suraj142005@gamil.com>
2024-08-28 06:46:03 +0530

    init

---
+++ app/Navbar/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+function page() {
+  return (
+    <div>
+        <div>
+            <Link><Img src="/menu.png"></Img></Link>
+        </div>
+    </div>
+  )
+}
+
+export default page
