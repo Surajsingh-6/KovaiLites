4c8c32f54c942c5f3cfc0735f607088499fa18f8
Suraj singh <suraj142005@gamil.com>
2024-08-30 22:29:11 +0530

    bugs fixing and deleting

--- pages/api/debug.js
+++
@@ -1,6 +0,0 @@
-// pages/api/debug-env.js
-export default function handler(req, res) {
-    console.log("Working with Mongo URI:", process.env.MONGODB_URI);
-    res.status(200).json({ mongoUri: process.env.MONGODB_URI });
-  }
-  
